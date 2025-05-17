93f5656e1a7e5e8a0f137c45ceeef2c42b3782ad
Nigel Khasiani <118476871+NigzMaf1a@users.noreply.github.com>
2025-05-17 23:31:12 +0300

    Update AccountantDashboardFragment.java

--- AccountantDashboardFragment.java
+++ AccountantDashboardFragment.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-ggg fff ttt rrr jjj
+ggg fff ttt rrr
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
